714eb6febc0f1a044ea59e9c572c0557a9feb37e
Vishnu <vishnu.garg@shephertz.co.in>
2014-07-03 16:44:36 +0530

    New AppHype Sdk 1.0
    Signed-off-by: Vishnu <vishnu.garg@shephertz.co.in>

--- Sample/src/com/example/apphypesample/SampleAppActivity.java
+++ Sample/src/com/example/apphypesample/SampleAppActivity.java
@@ -22,8 +22,6 @@ public class SampleAppActivity extends Activity implements AppHypeListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//Keys for test server
-	
 			AppHypeAPI
 			.intialize(
 					this,
